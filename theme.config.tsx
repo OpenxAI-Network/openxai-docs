692ffffbbf51d50af7775e2c4ba9b853e8356d6c
Plopmenz <plopmenz@gmail.com>
2025-09-10 14:49:28 +0200

    update github repo

--- theme.config.tsx
+++ theme.config.tsx
@@ -1,12 +1,12 @@
 // theme.config.tsx
-import React from 'react'
-import { DocsThemeConfig } from 'nextra-theme-docs'
-import { FaTelegramPlane } from 'react-icons/fa';
-import { FaXTwitter } from 'react-icons/fa6';
+import React from "react";
+import { DocsThemeConfig } from "nextra-theme-docs";
+import { FaTelegramPlane } from "react-icons/fa";
+import { FaXTwitter } from "react-icons/fa6";
 
 const config: DocsThemeConfig = {
   logo: <span>OpenxAI Docs</span>,
-  docsRepositoryBase: 'https://github.com/johnforfar/openxai-docs',
+  docsRepositoryBase: "https://github.com/OpenxAI-Network/openxai-docs/tree/main",
   head: (
     <>
       <meta name="viewport" content="width=device-width, initial-scale=1.0" />
@@ -16,47 +16,47 @@ const config: DocsThemeConfig = {
   ),
   sidebar: {
     //defaultMenuCollapseLevel: 1,
-    toggleButton: true
+    toggleButton: true,
   },
   navbar: {
     extraContent: (
       <>
-        <a 
-          href="https://x.com/OpenxAINetwork" 
-          target="_blank" 
+        <a
+          href="https://x.com/OpenxAINetwork"
+          target="_blank"
           rel="noopener noreferrer"
-          style={{ 
-            display: 'inline-block',
-            marginRight: '16px',
-            verticalAlign: 'middle'
+          style={{
+            display: "inline-block",
+            marginRight: "16px",
+            verticalAlign: "middle",
           }}
         >
           <FaXTwitter size={24} />
         </a>
-        <a 
-          href="https://t.me/OpenxAINetwork" 
-          target="_blank" 
+        <a
+          href="https://t.me/OpenxAINetwork"
+          target="_blank"
           rel="noopener noreferrer"
-          style={{ 
-            display: 'inline-block',
-            marginRight: '16px',
-            verticalAlign: 'middle'
+          style={{
+            display: "inline-block",
+            marginRight: "16px",
+            verticalAlign: "middle",
           }}
         >
           <FaTelegramPlane size={24} />
         </a>
       </>
-    )
+    ),
   },
   footer: {
-    content: 'OpenxAI Docs'
+    content: "OpenxAI Docs",
   },
   darkMode: true,
   nextThemes: {
-    defaultTheme: 'light',
-    storageKey: 'theme'
+    defaultTheme: "light",
+    storageKey: "theme",
   },
-  navigation: false
-}
+  navigation: false,
+};
 
-export default config
+export default config;
