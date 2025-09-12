a77e99c39d1fa35c4387f23e210084546ddcca88
Samuel Mens <45976616+Plopmenz@users.noreply.github.com>
2025-09-12 10:24:53 +0200

    Update _meta.ts

--- pages/web3/_meta.ts
+++ pages/web3/_meta.ts
@@ -1,6 +1,5 @@
 // auto-generated _meta.ts for web3-features
 export default {
-  "openxai-token": "OPENX Token",
   "token-utility": "Token Utility",
   "staking-system": "Staking System",
   "rewards": "Rewards",
